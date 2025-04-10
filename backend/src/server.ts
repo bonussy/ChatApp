f85b69ac1c59c4f911eeae6c0c11086360898a9e
Warintorn Surapolchai <poohmon.bonus@gmail.com>
2025-04-10 11:42:58 +0700

    fix socket server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,11 @@
 import express from 'express';
 import http from 'http';
-import { Server } from 'socket.io';
 import cors from 'cors';
 import connectDB from './config/db';
 import authRoutes from './routes/authRoutes';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
-import server from './socket/index';
+import initializeSocket from './socket';
 
 // Load environment variables
 dotenv.config();
@@ -17,14 +16,18 @@ connectDB();
 // Initialize Express app
 const app = express();
 app.use(express.json());
-
 app.use(cors());
-
 app.use(cookieParser());
 
 // Routes
 app.use('/api/auth', authRoutes);
+console.log('Auth routes loaded');
 
+// Create HtTTP server
+const server = http.createServer(app);
+
+// Initialize Socket.IO
+initializeSocket(server);
 
 // Start the server and Socket.io
 const PORT = process.env.PORT || 3001;
--- backend/src/socket/index.ts
+++ backend/src/socket/index.ts
@@ -1,35 +1,32 @@
 import { Server } from 'socket.io';
-import { createServer } from 'http';
-import express from 'express';
+import { Server as HttpServer } from 'http';
 
-const app = express();
-const server = createServer(app);
-
-//Create Socket.io server instance
-const io = new Server(server, {
-    cors: {
-        origin: '*',
-        methods: ['GET', 'POST'],
-        credentials: true,
-    },
-});
-
-//Handle socket connection event
-io.on('connection', (socket) => {
-    console.log('New client connected:', socket.id);
-
-    // You can listen for events like "message" to handle chat messages
-    socket.on('message', (data) => {
-        console.log('Message received:', data);
-        io.emit('message', data); // Broadcast message to all connected clients
+const initializeSocket = (server: HttpServer): void => {
+    //Create Socket.io server instance
+    const io = new Server(server, {
+        cors: {
+            origin: 'http://localhost:3000',
+            methods: ['GET', 'POST'],
+            credentials: true,
+        },
     });
 
-    
+    //Handle socket connection event
+    io.on('connection', (socket) => {
+        console.log('New client connected:', socket.id);
 
-    // Handle disconnection event
-    socket.on('disconnect', () => {
-        console.log('Client disconnected:', socket.id);
+        // Handle "message" event
+        socket.on('message', (data) => {
+            console.log('Message received:', data);
+            io.emit('message', data); // Broadcast message to all connected clients
+        });
+        
+        // Handle disconnection event
+        socket.on('disconnect', () => {
+            console.log('Client disconnected:', socket.id);
+        });
     });
-});
 
-export default server;
+};
+
+export default initializeSocket;
